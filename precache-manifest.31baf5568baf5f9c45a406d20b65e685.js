cea3b6ef017c8a533175f238bff0570031f5a281
Roman <kamazaff22@gmail.com>
2019-10-06 21:32:00 +0300

    add gh-pages

---
+++ precache-manifest.31baf5568baf5f9c45a406d20b65e685.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54606f170fc0a60e053574f5b6791300",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "c8a0e29a97fa0558b772",
+    "url": "/todo-app/static/css/main.7f2f5cef.chunk.css"
+  },
+  {
+    "revision": "0f5b8c770a970824b185",
+    "url": "/todo-app/static/js/2.a73d3059.chunk.js"
+  },
+  {
+    "revision": "c8a0e29a97fa0558b772",
+    "url": "/todo-app/static/js/main.f1c8cc6f.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
-import {todoReducer} from "./todo-reducer";
-import thunkMiddleware from "redux-thunk";
-
-let reducers = combineReducers({
-    todo: todoReducer
-});
-
-export const store = createStore(reducers, applyMiddleware(thunkMiddleware));
